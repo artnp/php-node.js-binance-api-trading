c59cd2fbbb2e21f15616ba1f0d2a2f9797c3e6e0
$artnp <99397593+artnp@users.noreply.github.com>
2022-10-27 11:33:56 +0700

    Add files via upload

--- saving.js
+++ saving.js
@@ -47,7 +47,7 @@ const getBtcData = async () => {
 
 	RealTimeProfit = (((lastPrice - average)*ExecutedCryptoSaving) - (((lastPrice - average)*ExecutedCryptoSaving)*fee)).toFixed(2);
 	// ปุ่มพากันหาย
-if (average === 0 && total === 0){
+if (average === 0 || total === 0 || total === NaN){
 	document.getElementById('ssbutton').style.display = 'none';
 } else if (realproF < 10){
 	document.getElementById('bbbutton').style.display = 'none';
